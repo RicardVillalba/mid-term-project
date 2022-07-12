c957e9d122c207a2209c45b2490e68389bb217eb
Ricard Villalba <ricardax@gmail.com>
2022-07-12 21:50:53 +0200

    rendering projects cards from array

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
-const myArray = ["stone","paper","scissors"];
-const embedElements = () => {
-   myArray.forEach(element => {
-      document.getElementById('result').innerHTML +=
-      `<div>${element}</div><br />`;
-      // here result is the id of the div present in the DOM
-   });
-};
+const projects = [
+  {
+    img: "./assets/projects-section/1.jpg",
+    name: "Simplify",
+    description: "UI Design & App Development",
+    link: "#",
+  },
+  {
+    img: "./assets/projects-section/2.jpg",
+    name: "Dashcoin",
+    description: "Web Development",
+    link: "#",
+  },
+  {
+    img: "./assets/projects-section/3.jpg",
+    name: "Vectorify",
+    description: "User Experience Design",
+    link: "#",
+  },
+];
+
+for (let i = 0; i < projects.length; i++) {
+  let projectName = document.createElement("p");
+  projectName.innerHTML = projects[i].name ;
+  let listname = document.getElementById("name");
+  
+
+  let projectDescription = document.createElement("p");
+  projectDescription.innerHTML = projects[i].description;
+  let listDescription = document.getElementById("description");
+  
+
+  let projectLink = document.createElement("p");
+  projectLink.innerHTML = projects[i].link;
+  let listLink = document.getElementById("link");
+
+  listname.appendChild(projectName);
+  listDescription.appendChild(projectDescription);
+  listLink.appendChild(projectLink);
+}
+
