f79cbdd0d33e21479c6ec105db3a89550e4a02be
Ricard Villalba <ricardax@gmail.com>
2022-07-13 13:44:40 +0200

    rendering projects cards from array with template string

--- index.js
+++ index.js
@@ -18,24 +18,51 @@ const projects = [
     link: "#",
   },
 ];
+const card = document.querySelector('#cards');
+projects.forEach((project)=>{
+  card.innerHTML=`
+  <div>
+  <img src="${project.img}" alt="">
+  <h3>${project.name}</h3>
+  <p>${project.description}</p>
+  <a href="${project.link}"></a>
+</div>
+  `
+  console.log(project);
+})
 
-for (let i = 0; i < projects.length; i++) {
-  let projectName = document.createElement("p");
-  projectName.innerHTML = projects[i].name ;
-  let listname = document.getElementById("name");
+
   
 
-  let projectDescription = document.createElement("p");
-  projectDescription.innerHTML = projects[i].description;
-  let listDescription = document.getElementById("description");
+
+  // let projectName = document.createElement("p");
+  // projectName.innerHTML = projects[i].name ;
+  // let listname = document.getElementById("name");
   
 
-  let projectLink = document.createElement("p");
-  projectLink.innerHTML = projects[i].link;
-  let listLink = document.getElementById("link");
+  // let projectDescription = document.createElement("p");
+  // projectDescription.innerHTML = projects[i].description;
+  // let listDescription = document.getElementById("description");
+  
 
-  listname.appendChild(projectName);
-  listDescription.appendChild(projectDescription);
-  listLink.appendChild(projectLink);
-}
+  // let projectLink = document.createElement("p");
+  // projectLink.innerHTML = projects[i].link;
+  // let listLink = document.getElementById("link");
 
+  // listname.appendChild(projectName);
+  // listDescription.appendChild(projectDescription);
+  // listLink.appendChild(projectLink);
+
+
+
+  // for (let i = 0; i < projects.length; i++) {
+  //   const card = document.querySelector('#cards');
+  //   card.innerHTML=`
+  //   <img src="${projects.img}" alt="">
+  //   <h3>${projects.name}</h3>
+  //   <p>${projects.description}</p>
+  //   <a href="${projects.link}"></a>
+  //   `
+  //   console.log(projects);
+  //   }
+    
